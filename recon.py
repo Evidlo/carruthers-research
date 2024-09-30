57239551977a987c97529a9396af151dd363b05e
Evidlo <evan_copernicus@widloski.com>
2024-09-29 20:56:43 -0500

    retrievals with albedo

--- recon.py
+++ recon.py
@@ -38,22 +38,14 @@ items = product(
     [10], # num_obs
     [28], # window (days)
     ['spring'], # season
-    [1e2], # difflam
+    [1e2, 1e3, 1e4], # difflam
     [360], # integration time
-    [50], # grid shape
 )
 # items = [['spring', 1e-1]]
 
 # for season, difflam, t_op in items:
-for num_obs, win, season, difflam, t_op, gshp in items:
+for num_obs, win, season, difflam, t_op, in items:
     t.cuda.empty_cache()
-    # desc = f'circ_{difflam:.0e}_projection'
-    # desc = f'{season}_{mo}mo_{difflam:.0e}_fakenoise{t_op}_diffmean_grid{gshp}_lognew'
-    desc = f'{win:02d}d_{num_obs:02d}obs_{{noise_type}}{t_op//60}hr_{difflam:.0e}_{season}'
-    # desc = f'{season}_{mo}mo_{difflam:.0e}_realnoise{t_op}'
-    # desc = f'{season}_{mo}mo_{difflam:.0e}_nonoise'
-    # desc = f'{season}_{mo}mo_{difflam:.0e}'
-    # desc = f'experiment'
 
     # integrate for full time between each snapshot
     # t_op = win * 24 / num_obs
@@ -67,23 +59,23 @@ for num_obs, win, season, difflam, t_op, gshp in items:
 
     # m = PratikModel(grid=None, date=season, rlim=(3, 25), device=device); mstr = 'pratik'
     # m = GonzaloModel(grid=None, device=device); mstr = 'gonzalo'
-    m = Zoennchen24Model(grid=default_grid((500, 50, 50), spacing='log'), device=device); mstr = 'zoennchen'
+    grid = BiResGrid((200, 60, 80), ce=30, ca=30, angle=45, spacing='log')
+    m = Zoennchen24Model(grid=grid, device=device); mstr = 'zoennchen'
     truth = m()
 
+    # cal = Calibrator(..., scene=Scene(iph=False))
 
     f = ForwardSph(
-        sc, m.grid,
-        use_albedo=(ual:=False), use_aniso=(uan:=True), use_noise=(uno:=True),
+        sc, m.grid, # calibrator=cal
+        use_albedo=(ual:=True), use_aniso=(uan:=True), use_noise=(uno:=True),
         science_binning=(sb:=True), device=device
     )
 
-    desc += f'_emask_noal_tshp{m.grid.shape.r}_frac{"x".join(map(str, f.vg[0].shape))}_wfi5Re'
 
-    # meas = f.noise(truth); noise_type = 'real'
-    meas = f.noise(truth, disable_noise=True); noise_type = 'realdisabled'
+    meas = f.noise(truth); noise_type = 'real'
+    # meas = f.noise(truth, disable_noise=True); noise_type = 'realdisabled'
     # meas = f(truth); noise_type = 'noiseless
     # meas = f.fake_noise(truth); noise_type='fake'
-    desc = desc.format(noise_type=noise_type)
 
     print('-----------------------------')
     print(desc)
@@ -96,24 +88,29 @@ for num_obs, win, season, difflam, t_op, gshp in items:
     # mr = FullyDenseModel(m.grid)
     # choose a model for retrieval
     # mr = SphHarmModel(default_grid(size_r=m.grid.size.r, shape=(49, 1, 3)), max_l=3, device=device)
-    mr = SphHarmModel(default_grid(shape=(gshp,) * 3, spacing='log'), device=device, max_l=3)
-    mr.proj = lambda coeffs: coeffs
     fr = ForwardSph(
-        sc, mr.grid,
+        sc, grid,
         use_albedo=ual, use_aniso=uan, use_noise=uno,
         science_binning=sb, device=device
     )
+    # %% debug
+    mr = SphHarmModel(grid, device=device, max_l=3)
+    mr.proj = lambda coeffs: coeffs
     # fr = f
     # choose loss functions and regularizers with weights
     loss_fns = [
         1 * SquareLoss(projection_mask=fr.pm),
         1e4 * NegRegularizer(),
-        diff_err:=difflam * DiffLoss(mr.grid, mode='density', device=device, _req='sq_diff_log')
+        differr:=difflam * DiffLoss(mr.grid, mode='density', device=device, _reg='sq_diff_log')
+        # differr:=1e-1 * DiffLoss(mr.grid, mode='density', device=device, _reg='gshp_diff_log')
+        # differr:=difflam * DiffLoss(mr.grid, mode='density', device=device, _reg='log_corr_diff')
     ]
-    #%% recon2
     # loss_fns = [CheaterLoss(truth)]
     loss_fns += [req_err := ReqErr(truth, m.grid, mr.grid, interval=100)]
-    coeffs, retrieved_meas, losses = gd(fr, meas, mr, lr=5e0, loss_fns=loss_fns, num_iterations=90000)
+    coeffs, retrieved_meas, losses = gd(
+        fr, meas, mr, lr=5e0, coeffs=t.load('/tmp/coeffs.tr', weights_only=True),
+        loss_fns=loss_fns, num_iterations=90000
+    )
 
     retrieved = mr(coeffs)
     # zero out retrieval/truth below 3Re
@@ -130,8 +127,9 @@ for num_obs, win, season, difflam, t_op, gshp in items:
     retrieved_meas_error = t.zeros_like(meas)
     retrieved_meas_error[nonzero] = (retrieved_meas - meas)[nonzero] / meas[nonzero]
 
+    desc = f'{win:02d}d_{num_obs:02d}obs_{{noise_type}}{t_op//60}hr_{differr.lam:.0e}_{season}_init_{differr._reg}'
+    desc = desc.format(noise_type=noise_type)
     # ----- Plotting -----
-    # %% debug
     # %% plot
     from dech import *
 
@@ -173,7 +171,7 @@ for num_obs, win, season, difflam, t_op, gshp in items:
                 # Figure("Percent Err Obs", HTML(image_stack(retrieved_meas_error.cpu(), fr.vg).to_jshtml()))
             ],
             Code(code),
-            Code(inspect.getsource(diff_err.compute))
+            Code(inspect.getsource(differr.compute))
         ],
         css='.animation {height: 300px};'
     )
