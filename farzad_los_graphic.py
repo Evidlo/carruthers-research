795162a416072b07c42a7732078bbcd9eebc46ac
Evidlo <evan_copernicus@widloski.com>
2024-10-07 15:29:55 -0500

    updated gitignore

--- farzad_los_graphic.py
+++ farzad_los_graphic.py
@@ -6,11 +6,12 @@ import matplotlib
 matplotlib.use('Agg')
 plt.close('all')
 
-from glide.common_components.view_geometry import carruthers_orbit, CameraWFI, CameraNFI
+from glide.common_components.view_geometry import carruthers_orbit, CameraWFI, CameraNFI, gen_mission
 from glide.science.orbit import viewgeom2ts
 from glide.science.plotting import orbit_svg
 from glide.science.model import default_vol
 from glide.science.forward import viewgeom_endplane
+from dech import *
 
 
 
@@ -59,48 +60,69 @@ def add_headers(
             )
 
 
-fig, axs = plt.subplots(2, 4, figsize=(10, 5), constrained_layout=True)
 
-# ax_cam - plot aes for each camera
-# cam - camera
-# d - LOS downsample factor
-
-interval = 1
+intervals = 1, 15, 30, 90
 vol = default_vol()
 seasons = ('spring', 'summer', 'fall', 'winter')
 cams = (CameraWFI(), CameraNFI())
 
-for ax_cam, cam, d in zip(axs, cams, (16, 64)):
+figs = []
 
-    for ax, season in zip(ax_cam, seasons):
+for interval in intervals:
+    fig, axs = plt.subplots(2, 4, figsize=(10, 5), constrained_layout=True)
 
-        view_geoms = carruthers_orbit(start=season, duration=interval, cam=cam, num_obs=2)
+    fig_row = []
+    # ax_cam - plot axes for each camera
+    # cam - camera
+    # d - LOS downsample factor
+    for ax_cam, cam, d in zip(axs, cams, (16, 64)):
 
-        vgts = viewgeom2ts(view_geoms)
-        endplane_positions = viewgeom_endplane(vgts)
+        for ax, season in zip(ax_cam, seasons):
 
-        for c, sp, ep in zip(['b', 'r'], vgts.src_pos, endplane_positions):
-            # plot every 10th pixel of top row of pixels
-            for ep10 in ep[:, 0, ::d].T:
-                ax.axline(sp[:2], ep10[:2], color=c)
+            # generate LOS plot with only 2 observations
+            view_geoms = carruthers_orbit(start=season, duration=interval, cam=cam, num_obs=2)
+            vgts = viewgeom2ts(view_geoms)
+            endplane_positions = viewgeom_endplane(vgts)
 
-            # ax.axis('equal')
-            width = 25
-            ax.set_xlim([-width, width])
-            ax.set_ylim([-width, width])
-            ax.grid(True
-                    )
-            ax.minorticks_on()
+            for c, sp, ep in zip(['b', 'r'], vgts.src_pos, endplane_positions):
+                # plot every 10th pixel of top row of pixels
+                for ep10 in ep[:, 0, ::d].T:
+                    ax.axline(sp[:2], ep10[:2], color=c)
 
-for ax in axs[1]:
-    ax.set_xlabel('Re')
+                # ax.axis('equal')
+                width = 25
+                ax.set_xlim([-width, width])
+                ax.set_ylim([-width, width])
+                ax.grid(True
+                        )
+                ax.minorticks_on()
 
-add_headers(
-    fig,
-    row_headers=list(map(lambda c: c.camID, cams)),
-    col_headers=list(map(str.capitalize, seasons))
-)
-# plt.tight_layout()
-plt.suptitle(f'Interval: {interval}d')
-plt.savefig(f'/srv/www/los{interval}.png')
-orbit_svg(vol, vgts).save('/srv/www/out.gif')
+    for ax in axs[1]:
+        ax.set_xlabel('Re')
+
+    add_headers(
+        fig,
+        row_headers=list(map(lambda c: c.camID, cams)),
+        col_headers=list(map(str.capitalize, seasons))
+    )
+    # plt.tight_layout()
+    plt.suptitle(f'Interval: {interval}d')
+
+    # plt.savefig(f'/srv/www/los{interval}.png')
+    fig_row.append(Img(fig))
+    figs.append(fig_row)
+
+    fig_row = []
+    for season in seasons:
+        # plot orbit with more observations
+        view_geoms = gen_mission(50, carruthers_orbit, cams, start=season, duration=interval)
+        orbit_img_html = orbit_svg(vol, viewgeom2ts(view_geoms, downsample_nfi=True))._repr_html_()
+        fig_row.append(HTML(orbit_img_html))
+
+    figs.append(fig_row)
+
+
+# %% plot
+p = '/srv/www/display/los_evolution.html'
+Page(figs).save(p)
+print(f'Saved to {p}')
--- quickstart.py
+++ quickstart.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python3
 
+import torch as t
+import numpy as np
+
 from glide.common_components.view_geometry import *
+from glide.common_components.generate_view_geom import *
+from glide.common_components.camera import *
+from glide.common_components.spacecraft import *
 from glide.common_components.cam import *
+from glide.common_components.science_pixel_binning import *
 from glide.science.plotting import *
+from glide.science.plotting_sph import *
 from glide.science.forward import *
+from glide.science.forward_sph import *
 from glide.science.decorators import *
 from glide.science.orbit import *
 from glide.science.model import *
+from glide.science.model_sph import *
 from glide.science.recon import *
 from glide.validation.scene import *
 from glide.validation.instrument import *
 from glide.calibration import *
 from glide.common_components.cam import *
+from sph_raytracer import *
 import tomosipo as ts
 from datetime import *
 from astropy.time import Time
 
-import importlib.resources
-path = importlib.resources.files('glide') / 'validation/data_files/Date_12_29_2025/GLIDE_WFI_radiance_B.txt'
-I_exo = np.loadtxt(path).reshape((512, 512))
 
-sc = SpaceCraft('2025-06-01')
-sc.add_sensor(CameraWFI())
-sc.updatePositionFromFile(sc.date)
-sc.pointAtEarth()
+cams = [CameraL1BWFI(), CameraL1BNFI()]
+sc = gen_mission(num_obs=1, cams=cams)
+grid = DefaultGrid()
+m = GonzaloModel(grid=grid)
+g = grid = m.grid
+g50 = DefaultGrid((50, 50, 50))
+g500 = DefaultGrid((500, 50, 50))
+mr = SphHarmModel(grid, max_l=3)
+density = m()
+
 
 vol = default_vol(shape=100, size=50)
-grid_cart = vol2cart(vol)
-grid_sph = cart2sph(grid_cart)
-cams=[CameraWFI()]
-view_geoms = gen_mission(
-    orbit=carruthers_orbit,
-    num_obs=20, cams=cams,
-    start='2025-07-01', duration=30*3,
-)
+f = ForwardSph(sc, grid, use_aniso=True, use_albedo=False)
+
 # small_vol = default_vol(shape=100)
 
 # cm = CamMode('WFI', t_int=1)
 # cs = CamSpec(cm)
 # cam_modes, cam_specs, view_geoms = gen_mission(num_obs=1, cam_mode=cm, cam_spec=cs)
 
+# import importlib.resources
+# path = importlib.resources.files('glide') / 'validation/data_files/Date_12_29_2025/GLIDE_WFI_radiance_B.txt'
+# I_exo = np.loadtxt(path).reshape((512, 512))
 # cam = CameraWFI()
 # view_geoms = carruthers_orbit(num_obs=1, cam=cam)
 # cm, cs = cam.cam_mode, cam.cam_spec
