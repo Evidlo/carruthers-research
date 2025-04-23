2990656d11d922e99c1de2d091f42f9040d8d6d8
Evidlo <evan_copernicus@widloski.com>
2025-04-22 19:29:56 -0500

    add fast science pixel binning experiment

---
+++ science_pixel_fast.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from contexttimer import Timer
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import matplotlib
+matplotlib.use('Agg')
+
+from glide.common_components.science_pixel_binning import SciencePixelBinning
+from sph_raytracer.plotting import *
+
+from skimage.transform import warp_polar, warp
+
+# load pregenerated measurements from file
+d = np.load('science_pixel_fast.npy', allow_pickle=True)
+meas_wfi, meas_nfi = d.item().get('wfi'), d.item().get('nfi')
+
+N = 1024; meas_data = meas_nfi
+# N = 512; meas_data = meas_wfi
+scishape = np.array((50, 100))
+shape = np.array((N, N))
+center = shape / 2
+coords = np.stack(np.meshgrid(*(np.arange(s) for s in shape), indexing='ij'), axis=-1)
+meas = meas_data
+# meas = np.zeros(shape)
+# meas[6+N//2, :] = 1
+# meas = np.linalg.norm(coords - center, axis=-1)
+# meas = np.random.random(shape)
+# add artificial feature for alignment debugging purposes
+# meas[:N//4, :N//4] = 1
+# meas[:N//3, N//4:N//3] = 1
+
+# ----- Original Science Pixel Binning -----
+
+
+with Timer(prefix='Original') as p:
+    sb = SciencePixelBinning(N, scishape, rlim=(0, N/2), normalize=True)
+
+    meas_sb = sb(meas[None])
+
+
+# ----- Fast Science Pixel Binning -----
+
+# plt.imsave('/www/out.png', meas)
+
+with Timer(prefix='Fast') as p:
+    oversample = 10
+    """
+    meas_warp = warp_polar(
+        meas,
+        output_shape=np.flip(scishape * oversample),
+        radius=N//2 + .125
+    ).T
+    """
+
+    def affine(x, xlim, ylim):
+        xlim, ylim = np.asarray(xlim), np.asarray(ylim)
+        normalized = (x - xlim[:, 0]) / (xlim[:, 1] - xlim[:, 0])
+        return normalized * (ylim[:, 1] - ylim[:, 0]) + ylim[:, 0]
+
+    rlim, thlim = sb.rlim, np.deg2rad(sb.thlim)
+    rlim += np.array((+0.05, 0))
+    outshape = scishape * oversample
+    def map_func(output_coords):
+
+        # warp(...) gives `output_coords` in (col, row) format for some reason
+        theta, r = affine(
+            output_coords + np.array((0.5, 0.5)),
+            [(0, outshape[1]), (0, outshape[0])],
+            [thlim, rlim]
+        ).T
+
+        xy = np.stack((r * np.cos(-theta), r * np.sin(-theta)), axis=-1)
+        uv = xy + sb.ctr - 0.5
+        # import ipdb
+        # ipdb.set_trace()
+        return uv
+
+    meas_warp = warp(
+        meas, map_func, output_shape=outshape, order=1
+    )
+
+
+    print(meas_warp.shape)
+    # meas_warp = np.roll(meas_warp, oversample*scishape[1]//2 - 0, axis=1)
+    # meas_warp = np.flip(meas_warp, axis=1)
+    meas_warp = meas_warp.reshape((scishape[0], oversample, scishape[1], oversample))
+    meas_warp = meas_warp.mean(axis=(-3, -1))
+    # meas_warp = meas_warp / oversample**2
+    print(meas_warp.shape)
+
+# ----- Plotting -----
+
+err = np.divide((meas_warp - meas_sb), meas_sb, where=meas_sb!=0) * 100
+err = np.where(meas_sb>1e-5, err, 0)
+
+plt.close('all')
+
+plt.figure(dpi=200, figsize=(18, 6))
+
+plt.subplot(2, 3, 1)
+plt.title('Orig')
+plt.imshow(meas)
+plt.subplot(2, 3, 4)
+plt.title('Science')
+plt.imshow(meas_sb, norm=LogNorm())
+plt.colorbar()
+plt.subplot(2, 3, 5)
+plt.title('Warp')
+plt.imshow(meas_warp, norm=LogNorm())
+plt.colorbar()
+plt.subplot(2, 3, 6)
+plt.title('Error %')
+plt.imshow(err, vmin=-2, vmax=2, cmap='seismic')
+plt.colorbar()
+
+plt.tight_layout()
+plt.savefig('/www/out.png')
